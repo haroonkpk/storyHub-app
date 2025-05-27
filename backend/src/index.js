47d8eea6e01dd355c044051fb72d2e29358bdabe
haroonkpk <haroonkhanlala47@gmail.com>
2025-05-27 15:26:22 +0500

    protectedRoute added

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import { connectDB } from './lib/db.js';
 import dotenv from 'dotenv';
 import authRoute from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+
 dotenv.config(); 
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser());
 
 
 
