f98db3e6e3929729ccb0487c0d200f7814d1844a
haroonkpk <haroonkhanlala47@gmail.com>
2025-06-01 13:24:59 +0500

    login, logout, signup conected to backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,13 +4,17 @@ import dotenv from 'dotenv';
 import authRoute from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import storyRoute from './routes/story.routes.js';
+import cors from 'cors';
 
 dotenv.config(); 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 
 app.use("/api/auth", authRoute);
 app.use("/api/storys", storyRoute);
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,7 +7,7 @@ export function generateToken(userId, res) {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 }
