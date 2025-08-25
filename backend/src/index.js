315e89a77cdffd9527cd316cb13c5ee89ad075de
haroonkpk <haroonkhanlala47@gmail.com>
2025-08-25 20:11:31 +0500

    Add production environment handling and static file serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,12 @@ import cookieParser from 'cookie-parser';
 import storyRoute from './routes/story.routes.js';
 import favoritesRoute from './routes/favorites.route.js';
 import cors from 'cors';
+import path from "path";
 
 dotenv.config(); 
+const __dirname = path.resolve();
+
+
 const app = express();
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
@@ -20,6 +24,16 @@ app.use(cors({
 app.use("/api/auth", authRoute);
 app.use("/api/story", storyRoute);
 app.use("/api/favorites", favoritesRoute);
+
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+ }
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
